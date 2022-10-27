07c6c48a4fd180445e632cac6129bc6047656d04
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-10-27 15:34:10 +0900

    ロジック作成 Fixed #40

--- src/db/db.ts
+++ src/db/db.ts
@@ -42,3 +42,19 @@ export const saveBudgetDB = (item: budgetType): void => {
       console.log(error);
     });
 };
+
+export const deleteBudgetDB = (): void => {
+  budgetDB
+    .transaction("rw", budgetDB.budget, async () => {
+      await budgetDB.budget.bulkPut([
+        { id: 0, category: "移動費", budgetDetails: [{ name: "", price: 0 }] },
+        { id: 1, category: "宿泊費", budgetDetails: [{ name: "", price: 0 }] },
+        { id: 2, category: "食費費", budgetDetails: [{ name: "", price: 0 }] },
+        { id: 3, category: "観光費", budgetDetails: [{ name: "", price: 0 }] },
+        { id: 4, category: "お土産代", budgetDetails: [{ name: "", price: 0 }] },
+      ]);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
